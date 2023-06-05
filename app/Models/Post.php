c6835dc600a805da12fedf095ccbe8ea049f7a24
ryuji0012 <joker.core@rakumail.jp>
2023-06-05 23:21:12 +0900

    file2

---
+++ app/Models/Post.php
@@ -0,0 +1,7 @@
+<?php
+
+public function getByLimit(int $limit_count = 10)
+{
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+}
