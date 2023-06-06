6fe8d28df41d0f9e804178b5975ef23bd8553ae4
ryuji0012 <joker.core@rakumail.jp>
2023-06-06 21:17:03 +0900

    file8-4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,3 +5,7 @@ public function getByLimit(int $limit_count = 10)
     // updated_atで降順に並べたあと、limitで件数制限をかける
     return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
 }
+protected $fillable = [
+    'title',
+    'body',
+];
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,6 +13,10 @@
                 <div class='post'>
                     <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
+                    <a href='/posts/create'>create</a>
+                    <h2 class='title'>
+                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
                 </div>
             @endforeach
         </div>
